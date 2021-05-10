d61f05b4028883ea5acf734a365da05fa8028632
zgjoget9 <zgjoget@163.com>
2021-05-10 11:37:59 +0800

    bio

--- about/js/main.js
+++ about/js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is M.Reza", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["My Name is Zhang Gaojie",  "Love Simplicity"],
         typeSpeed: 100,
         backDelay: 900,
         // loop
